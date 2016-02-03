13b90ccb84b1266095973a11bda1fbc717819908
Richard McSharry <richard.mcsharry@electricscribes.com>
2016-02-03 09:20:13 +0700

    Adds page 2 and text items

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+function loadPrev(){
+    $('.carousel').carousel('prev');
+}
+function loadNext(){
+    $('.carousel').carousel('next');
+}
+
+$('.portfolio-item').click(function() {
+        $(this).find('.image').css('opacity', function(i,o){
+        return parseFloat(o).toFixed(1) === '0.5' ? 1 : 0.5;
+    });
+        $(this).find('.text').slideToggle("slow");
+});
+
