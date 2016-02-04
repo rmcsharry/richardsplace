13fe92816be75670b34c474b708c7e100aa8d11c
Richard McSharry <richard@tripolis.com>
2016-02-04 18:00:46 +0700

    Fixes layout for mobile

--- portfolio.js
+++ portfolio.js
@@ -9,6 +9,6 @@ $('.portfolio-item').click(function() {
         $(this).find('.image').css('opacity', function(i,o){
         return parseFloat(o).toFixed(1) === '0.5' ? 1 : 0.5;
     });
-        $(this).find('.text').slideToggle("slow");
+        $(this).find('.text').slideToggle("fast");
 });
 
